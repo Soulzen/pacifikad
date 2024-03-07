222de323b15fb8a5daae83a04e563a014e1c0059
Soulzen <alberto.liria@gmail.com>
2024-03-07 19:51:27 +0100

    Anaylitics + SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 import { Quicksand, Open_Sans } from "next/font/google"
 import "./globals.css"
 import { Header } from "@/components/header/header"
@@ -30,6 +32,8 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   )
