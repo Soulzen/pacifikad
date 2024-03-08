74da782773b1f225b8adf39518060e8e6f58a277
Soulzen <alberto.liria@gmail.com>
2024-03-08 19:14:27 +0100

    changed to a minimalistic style footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -12,69 +12,26 @@ const Footer = () => {
             <Logo className={styles.icon} />
             Pacifikad
           </h3>
+
           <p className={styles.text}>
             © 2024 Pacifikad. Todos los derechos reservados.
           </p>
+        </div>
 
-          <p className={styles.text}>Correo electrónico: info@pacifikad.com</p>
-        </div>
         <div className={styles.subsection}>
-          <h3 className={styles.heading}>Enlaces</h3>
           <ul className={styles.list}>
-            <li>
-              <Link className={styles.link} href="/">
-                Inicio
-              </Link>
-            </li>
-            <li>
-              <Link className={styles.link} href="/servicios">
-                Servicios
-              </Link>
-            </li>
-            <li>
-              <Link className={styles.link} href="/contacto">
-                Contacto
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className={styles.subsection}>
-          <h3 className={styles.heading}>Siguenos en</h3>
-          <ul className={styles.list}>
-            <li>
-              <a className={styles.link} href="#">
-                <Image
-                  src="/social_media/instagram.svg"
-                  alt="instagram"
-                  width={30}
-                  height={30}
-                />{" "}
-                @pacifikad
-              </a>
-            </li>
-            <li>
-              <a className={styles.link} href="#">
-                <Image
-                  src="/social_media/facebook.svg"
-                  alt="facebook"
-                  width={30}
-                  height={30}
-                />{" "}
-                /pacifikad
-              </a>
-            </li>
             <li>
               <a
                 className={styles.link}
                 href="https://www.linkedin.com/in/makarena-ponce/"
               >
                 <Image
+                  className={styles.sm_icon}
                   src="/social_media/linkedin.svg"
                   alt="linkedin"
                   width={30}
                   height={30}
-                />{" "}
-                /in/makarena-ponce/
+                />
               </a>
             </li>
           </ul>
