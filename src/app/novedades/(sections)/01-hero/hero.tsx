0b46489694ad87f8863ae7c8a02035a15dfeaef9
Soulzen <alberto.liria@gmail.com>
2024-04-09 21:42:17 +0200

    created the hero section of the News page

---
+++ src/app/novedades/(sections)/01-hero/hero.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image"
+import styles from "./hero.module.css"
+import novedades_img from "@/../public/images/novedades.jpg"
+
+const Hero = () => {
+  return (
+    <section className={styles.section}>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <Image
+            className={styles.image}
+            src={novedades_img}
+            alt="smooth waves in the ocean"
+            fill
+            sizes="100vw"
+          />
+          <h1 className={styles.title}>
+            Hola, somos Pacifikad. Descubre nuestras estrategias y herramientas
+            de Marketing para potenciar tu Marca Online
+          </h1>
+          <p className={styles.subtitle}>
+            Sumérgete en la Profundidad Digital: Explora nuestro blog para
+            encontrar Estrategias de Marketing desde los Océanos Pacíficos
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
