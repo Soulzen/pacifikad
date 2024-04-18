ce72d0a401b2bb373c0d13402cff618755bc3844
Soulzen <alberto.liria@gmail.com>
2024-04-18 11:14:33 +0200

    fixed atributes for svg

--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -39,9 +39,9 @@ const Pagination = ({ pagination }: PaginationProps) => {
             >
               <path
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M13 5H1m0 0 4 4M1 5l4-4"
               />
             </svg>
@@ -58,9 +58,9 @@ const Pagination = ({ pagination }: PaginationProps) => {
             >
               <path
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M13 5H1m0 0 4 4M1 5l4-4"
               />
             </svg>
@@ -82,9 +82,9 @@ const Pagination = ({ pagination }: PaginationProps) => {
             >
               <path
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M1 5h12m0 0L9 1m4 4L9 9"
               />
             </svg>
@@ -101,9 +101,9 @@ const Pagination = ({ pagination }: PaginationProps) => {
             >
               <path
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M1 5h12m0 0L9 1m4 4L9 9"
               />
             </svg>
